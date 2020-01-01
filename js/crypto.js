3246e58f42a75cc741a63810e4511c60b9765c80
Malavika <gs.malavika@yahoo.com>
2020-01-01 17:06:08 +0000

    api key added
    API key and URL added, files renamed and unwanted image file moved

---
+++ js/crypto.js
@@ -0,0 +1,3 @@
+const apiKey = "7281d41481632ac57ab5f3c97ce31de1fea0d2dad356cd2ffa71b4e2f0ec2256";
+const apiURL = "https://min-api.cryptocompare.com/data/exchange/top/volume?e=Binance&direction=TO";
+const apiURLNews ="https://min-api.cryptocompare.com/data/v2/news/?lang=EN"
