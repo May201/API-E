9633c407159518bc7506cc9e902edd43b89d5472
Malavika <gs.malavika@yahoo.com>
2020-01-02 19:32:58 +0000

    News feed sourced via API
    adding news feed via api to the application on load

--- js/crypto.js
+++ js/crypto.js
@@ -1,3 +1,33 @@
-const apiKey = "7281d41481632ac57ab5f3c97ce31de1fea0d2dad356cd2ffa71b4e2f0ec2256";
-const apiURL = "https://min-api.cryptocompare.com/data/exchange/top/volume?e=Binance&direction=TO";
-const apiURLNews ="https://min-api.cryptocompare.com/data/v2/news/?lang=EN"
+const apiKey =
+    "7281d41481632ac57ab5f3c97ce31de1fea0d2dad356cd2ffa71b4e2f0ec2256";
+const apiURL =
+    "https://min-api.cryptocompare.com/data/exchange/top/volume?e=Binance&direction=TO";
+const apiURLNews = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN";
+
+//to get the news from the api on page load
+
+$(document).ready(function() {
+    $.ajax({
+        url: apiURLNews,
+        method: "GET",
+        dataType: "JSON",
+        success: function(news) {
+            let output = "";
+
+            let latestNews = news.Data;
+            latestNews.forEach(element => {
+                output += `
+                <div class="col-md-4 col-sm-12">
+                    <div class="card">
+                        <h6>${element.title}</h6>
+                    </div>
+                </div>
+            `;
+            });
+
+            if (output !== "") {
+                $("#newsResult").html(output);
+            }
+        },
+    });
+});
