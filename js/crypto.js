82a4bfff71f6f077ea2ffe8bb8e84fc77bde3302
Malavika <gs.malavika@yahoo.com>
2020-01-12 21:04:33 +0000

    Updates for pagination

--- js/crypto.js
+++ js/crypto.js
@@ -7,7 +7,6 @@ const currencyUrl =
     "https://min-api.cryptocompare.com/data/top/totaltoptiervolfull?limit=10&tsym=USD";
 
 //to get the news from the api
-
 $(document).ready(function() {
     $.ajax({
         url: apiURLNews,
@@ -15,11 +14,12 @@ $(document).ready(function() {
         dataType: "JSON",
         success: function(news) {
             let output = "";
+            // let pagination;
 
             let latestNews = news.Data;
             latestNews.forEach(element => {
                 output += `
-                <div class="col-md-4 col-sm-12">
+                <div class="col-md-4 col-sm-12 news-item">
                     <div class="card h-100 w-100">
                         <h6>${element.title}</h6>
                         <img src="${element.source_info.img}">
@@ -31,13 +31,72 @@ $(document).ready(function() {
             if (output !== "") {
                 $("#newsResult").html(output);
             }
-        },
 
-        error: function () {
+            //pagination
+            const numberofItems = $("#newsResult").children().length;
+            const pageSize = 3;
+
+            $(`#newsResult .news-item:gt(${pageSize - 1})`).hide();
+
+            const totalPages = Math.ceil(numberofItems / pageSize);
+
+            for (i = 1; i <= totalPages; i++) {
+                $(".pagination").append(`<li class="page-item">
+                    <a class="page-link" href="#"> ${i} </a>
+                </li>`);
+            }
+
+            $(".pagination").append(`<li class="page-item">
+                <a class="page-link" href="#">Next</a>
+            </li>`);
+
+            $(".pagination li").on("click", function() {
+                // Removing the "active" class from all of the paginatione elements
+                $(".pagination li").removeClass("active");
+
+                // Add the "active" class to the currently clicked element
+                $(this).addClass("active");
+
+                // Current selected page number
+                const currentPage = $(this).index();
+
+                // Calculate the start and end index for the current page
+                const currentPageStartIndex = currentPage * pageSize - 1;
+                const currentPageEndIndex = currentPageStartIndex + pageSize;
+
+                // Hide all the loaded news elements
+                $("#newsResult .news-item").hide();
+
+                // Show only the elements for the currently selected page
+                for (
+                    let i = currentPageStartIndex;
+                    i < currentPageEndIndex;
+                    i++
+                ) {
+                    $(`#newsResult .news-item:eq(${i})`).show();
+                }
+            });
+            // function createPagination(nxt,prev){
+            //     if(nxt && prev ){
+            //         return `<button onclick="writeToDocument('${prev}')">Previous</button>
+            //                 <button onclick="writeToDocument('${nxt}')">Next</button>`;
+
+            //     }else if (nxt && !prev){
+            //         return `<button onclick="writeToDocument('${nxt}')">Next</button>`;
+            //     }else if (!nxt && prev){
+            //         return `< button onclick = "writeToDocument('${prev}')"> Previous</button >`;
+            //     }
+            // }
+            //             if(latestNews.next || latestNews.previous){
+            //                 pagination= createPagination(latestNews.next,latestNews.previous)
+            //             }
+        },
+        error: function() {
             $("#newsResult").html(
                 "The news cannot be displayed now due to an error. Please try later."
-            )
-    };
+            );
+        },
+    });
 });
 
 // To get top 10 cryptocurrencies
