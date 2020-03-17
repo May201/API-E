61a46c27fee3cb14aa62546dfcbaf9b7bc9560ae
Malavika <gs.malavika@yahoo.com>
2020-03-17 19:19:20 +0000

    Add more tests

--- js/spec/cryptoSpec.js
+++ js/spec/cryptoSpec.js
@@ -11,6 +11,32 @@ describe("Crypto API Tests", function() {
     $("#test-container").append(elements);
   }
 
+  it("display news", function() {
+    addTestElements(`<div id='newsResult'></div>`);
+    const dummyAPIResponse = {
+      Data: [
+        {
+          url: "some-url-1",
+          title: "some-title-1",
+          source_info: { img: "some-img-1" }
+        },
+        {
+          url: "some-url-2",
+          title: "some-title-2",
+          source_info: { img: "some-img-2" }
+        }
+      ]
+    };
+    displayNews(dummyAPIResponse);
+    const result = $("#newsResult").html();
+    expect(result).toContain("some-url-1");
+    expect(result).toContain("some-title-1");
+    expect(result).toContain("some-img-1");
+    expect(result).toContain("some-url-2");
+    expect(result).toContain("some-title-2");
+    expect(result).toContain("some-img-2");
+  });
+
   it("fetch crypto currencies", function() {
     // Create a new DIV to the Jasmine test page
     addTestElements("<div id='CryptoType'></div>");
@@ -68,5 +94,14 @@ describe("Crypto API Tests", function() {
     expect($(".result").css("display")).toBe("none");
   });
 
-  it("will validate currency selections", function() {});
+  it("will validate currency selections", function() {
+    addTestElements(`
+      <select id='CryptoType'></select>
+      <select id='CurrencyType'></select>
+      <button id='submitBtn'>Submit</button>
+    `);
+    const alertSpy = spyOn(window, "alert");
+    submitBtnHandler();
+    expect(alertSpy).toHaveBeenCalledWith("Please choose both values!");
+  });
 });
